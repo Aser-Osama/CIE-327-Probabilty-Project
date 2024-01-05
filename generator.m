2845721e460580ef917cf417de5b202c1d664b52
CHEX <52354962+SFCHEX@users.noreply.github.com>
2024-01-05 17:24:44 +0200

    asers function instead of mine :(

--- generator.m
+++ generator.m
@@ -1,13 +1,6 @@
-% Function to generate time vector
 generateTimeVector = @(start, endValue, numPoints) linspace(start, endValue, numPoints);
-
-% Generate time vector with more points and a larger time span
-t = generateTimeVector(0, 40, 4001);  % 4000 points over 40 units of time
-
-% Parameters
-num_samples = 4001;  % 4000 samples
-
-% Ensemble Z(t)
+t = generateTimeVector(0, 40, 101);
+num_samples = 101;
 ensemble_Z = zeros(num_samples, length(t));
 
 for i = 1:num_samples
@@ -16,25 +9,17 @@ for i = 1:num_samples
     ensemble_Z(i, :) = Z_t;
 end
 
-% Save ensemble Z(t) to a MAT file
-save('ensemble_Z.mat', 'ensemble_Z', 't');
-
-% Ensemble W(t)
 ensemble_W = zeros(num_samples, length(t));
 
 for i = 1:num_samples
-    A = -5 + 5 * randn;  % Generating normally distributed random amplitude
+    A = -5 + 5 * randn;
     W_t = A * cos(4 * pi * t);
     ensemble_W(i, :) = W_t;
 end
 
-% Save ensemble W(t) to a MAT file
-save('ensemble_W.mat', 'ensemble_W', 't');
 
-% Rename variables for X and t
 X = ensemble_Z;
-t = t;
-
-% Save ensembles with X and t
 save('ensemble_Z.mat', 'X', 't');
+X = ensemble_W;
 save('ensemble_W.mat', 'X', 't');
+
